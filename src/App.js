cedefe1d0f7d7c11662e9acaf2d221cc2acb4992
Vladimir <Shamoointhedark@gmail.com>
2021-07-05 16:34:19 +0300

    added cart section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div style={{display: 'none'}} className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30 d-flex justify-between">Корзина <img className="removeBtn cu-p" src="/content/btn-remove.svg" alt="btn-remove"/></h2>
+
+          <div className="items">
+            <div className="cartItem d-flex justify-between align-center mb-20">
+              <img width={70} height={70} className="cardItemImg" src="/content/cards/boucket-orchid.jpg" alt="orchid"/>
+              <div className="mr-20">
+                <p className="mb-5">Букет «Орхи»</p>
+                <b>3 460р.</b>
+              </div>
+              <img className="removeBtn" src="/content/btn-remove.svg" alt="btn-remove"/>
+            </div>
+            <div className="cartItem d-flex justify-between align-center mb-20">
+            <img width={70} height={70} className="cardItemImg" src="/content/cards/eleven-white-roses.jpg" alt="11 roses"/>
+              <div className="mr-20">
+                <p className="mb-5">11 белых роз</p>
+                <b>1 860р.</b>
+              </div>
+              <img className="removeBtn" src="/content/btn-remove.svg" alt="btn-remove"/>
+            </div>
+          </div>
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>5 630р.</b>
+              </li>
+              <li>
+              <span>Налог 5%:</span>
+                <div></div>
+                <b>282р.</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ 
+              <img src="/content/arrow-btn.svg" alt="arrow-btn"/>
+            </button>
+          </div>
+        
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} heigh={40} src="/content/logo2.svg" alt="logo" />
@@ -21,10 +66,18 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все букеты</h1>
-        
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все букеты</h1>
+          <div className="search-block">
+            <img src="/content/search.svg" alt="search"/>
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
         <div className="d-flex">
           <div className="card">
+            <div className="favourite">
+              <img src="/content/unliked.svg" alt="liked"/>
+            </div>
             <img width={135} height={115} src="/content/cards/elegant.jpg" alt="elegant-flower"></img>
             <h5>Букет «Элегантный»</h5>
             <div className="d-flex justify-between align-center">
@@ -33,7 +86,7 @@ function App() {
                 <b>2 170р.</b>
               </div>
                 <button className="button">
-                  <img width={11} height={11} src="/content/plus.svg" alt="plus"></img>
+                <img src="/content/unchecked.svg" alt="unchecked"></img>
                 </button>
             </div>
           </div>
@@ -47,7 +100,7 @@ function App() {
                 <b>2 680р.</b>
               </div>
                 <button className="button">
-                  <img width={11} height={11} src="/content/plus.svg" alt="plus"></img>
+                <img src="/content/checked.svg" alt="checked"></img>
                 </button>
             </div>
           </div>
@@ -61,7 +114,7 @@ function App() {
                 <b>3 460р.</b>
               </div>
                 <button className="button">
-                  <img width={11} height={11} src="/content/plus.svg" alt="plus"></img>
+                <img src="/content/unchecked.svg" alt="unchecked"></img>
                 </button>
             </div>
           </div>
@@ -75,7 +128,7 @@ function App() {
                 <b>1 860р.</b>
                 </div>
                 <button className="button">
-                  <img width={11} height={11} src="/content/plus.svg" alt="plus"></img>
+                <img src="/content/unchecked.svg" alt="unchecked"></img>
                 </button>
             </div>
           </div>
